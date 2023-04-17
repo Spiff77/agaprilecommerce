3fc7dd990663d37e50f262a7c332804e315fd4a1
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-04-17 17:52:47 +0300

    input

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from '../model/product.model';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+
+  product1: Product = {
+    id : 1,
+    name: 'Frank Zappa',
+    category : 'Album',
+    active: true,
+    description: 'Un cd qu\'il est bien pour l\'écouter',
+    price: 10,
+    promo: .2
+  }
+
+  product2: Product = {
+    id : 2,
+    name: 'Raoul petit',
+    category : 'Album',
+    active: true,
+    description: 'Un cd qu\'il est bien pour l\'écouter',
+    price: 12,
+    promo: .1
+  }
 
 }
