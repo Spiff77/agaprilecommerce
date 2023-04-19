d42f7843db386536c6acd7a06e324251886a83d2
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-04-19 12:46:05 +0300

    NotificationServiceUpdate

--- src/app/components/selected-box/selected-box.component.ts
+++ src/app/components/selected-box/selected-box.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
 import {Product} from '../../model/product.model';
 import {ProductService} from '../../service/product.service';
 import {NotificationService} from '../../service/notification.service';
@@ -8,12 +8,14 @@ import {NotificationService} from '../../service/notification.service';
   templateUrl: './selected-box.component.html',
   styleUrls: ['./selected-box.component.css']
 })
-export class SelectedBoxComponent {
+export class SelectedBoxComponent implements OnChanges{
 
   @Input()
   selectedProduct: Product | undefined;
   showConfirmDelete = false;
 
+  @Output()
+  onCloseBox = new EventEmitter<void>()
 
   constructor(private ps: ProductService, private ns: NotificationService) {}
 
@@ -21,9 +23,19 @@ export class SelectedBoxComponent {
     this.ps.delete(this.selectedProduct?.id)
       .subscribe(() => {
         this.selectedProduct = undefined
-        this.showConfirmDelete = false;
-        this.ns.emitter.next();
+        this.showConfirmDelete = false
+        this.ns.emitter.next()
       })
+  }
+
+  closeBox(){
+    this.onCloseBox.emit()
+    this.selectedProduct = undefined
+    this.showConfirmDelete = false
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.showConfirmDelete = false
   }
 
 }
