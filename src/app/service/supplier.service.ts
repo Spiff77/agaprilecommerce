b825f37c974a16449d8deb5b2988e287271f7bbc
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-04-20 10:41:16 +0300

    supplier.service.ts

---
+++ src/app/service/supplier.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Supplier} from '../model/supplier.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupplierService {
+
+  private url = 'http://localhost:8080/learn/api/suppliers';
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Supplier[]>{
+    return this.http.get<Supplier[]>(this.url)
+  }
+  findOne(id: number): Observable<Supplier>{
+    return this.http.get<Supplier>(`${this.url}/${id}`)
+  }
+  delete(id: number|undefined): Observable<Supplier[]>{
+    return this.http.delete<Supplier[]>(`${this.url}/${id}`)
+  }
+  add(supplier: Supplier): Observable<Supplier>{
+    return this.http.post<Supplier>(`${this.url}`, supplier)
+  }
+}
